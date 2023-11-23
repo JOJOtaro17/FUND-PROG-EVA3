2e87b7726fdad9a18aa5a33b3be38bab30d1c82e
JOJOtaro17 <143568674+JOJOtaro17@users.noreply.github.com>
2023-11-22 19:11:27 -0600

    Add files via upload

---
+++ EVA3_1_FUNCIONES/src/eva3_1_funciones/EVA3_1_FUNCIONES.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_funciones;
+
+/**
+ *
+ * @author Joshua
+ */
+public class EVA3_1_FUNCIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //LAMADA FUNCION
+        imprimirmensaje("hola mundo");
+    }
+    
+    
+    public static void imprimirmensaje(String mensaje){
+        System.out.println(mensaje);
+    }
+    
+}
